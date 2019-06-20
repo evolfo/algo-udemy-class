937b958644dd4724647655029fe5f396bab82a7d
Angelo Spampinato <spampinatodesign@gmail.com>
2019-06-19 21:25:15 -0400

    first commit

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,32 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split('').reduce((rev, char) => char + rev, '')
+}
 
 module.exports = reverse;
+
+
+//1st solution
+// function reverse(str) {
+//   return str.split('').reverse().join('')
+// }
+
+//2nd solution
+// function reverse(str) {
+//   let reversed = []
+//   for (let i = 0; i < str.length; i++) {
+//   	reversed.unshift(str[i])
+//   }
+//   return reversed.join('')
+// }
+
+//3rd solution
+// function reverse(str) {
+//   let reversed = ''
+//   for (let character of str) {
+//   	reversed = character + reversed;
+//   }
+//   return reversed;
+// }
